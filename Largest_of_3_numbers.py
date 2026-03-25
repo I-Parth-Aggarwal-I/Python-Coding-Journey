257ce690402cec841f516678c6c8d88f1b64360c
The-Lazy-Programmar <parthaggarwal2024@gmail.com>
2026-03-25 22:05:52 +0530

    single line input

--- Largest_of_3_numbers.py
+++ Largest_of_3_numbers.py
@@ -1,6 +1,9 @@
-n1 = float(input("Enter the first number: "))
-n2 = float(input("Enter the second number: "))
-n3 = float(input("Enter the third number: "))
+#n1 = float(input("Enter the first number: "))
+#n2 = float(input("Enter the second number: "))
+#n3 = float(input("Enter the third number: "))
+
+n1, n2, n3 = map(float, input("Enter three numbers separated by spaces: ").split())
+
 if n1 >= n2 and n1 >= n3:
     print(n1, "is the largest number.")
 elif n2 >= n1 and n2 >= n3:
