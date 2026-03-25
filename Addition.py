a6d1e6eeaf696443dd786fec9a4968faef056ef3
The-Lazy-Programmar <parthaggarwal2024@gmail.com>
2026-03-25 21:50:08 +0530

    addition with input

--- Addition.py
+++ Addition.py
@@ -8,3 +8,8 @@ str1 = "Hello"
 str2 = "World"
 str3 = str1 + " " + str2
 print(str3)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+print("The sum of", a, "and", b, "is", c)
