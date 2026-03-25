e5358ac073001b84942fb53df2b8923eca2281ab
The-Lazy-Programmar <parthaggarwal2024@gmail.com>
2026-03-25 21:55:06 +0530

    with & without temporary variable

---
+++ Swap.py
@@ -0,0 +1,16 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+print("Before swapping: n1 =", n1, "and n2 =", n2)
+
+# temp
+n3 = n1
+n1 = n2
+n2 = n3
+print("After swapping: n1 =", n1, "and n2 =", n2)
+
+# without temp
+n1 = n1 + n2
+n2 = n1 - n2
+n1 = n1 - n2
+print("After swapping without temp: n1 =", n1, "and n2 =", n2)
